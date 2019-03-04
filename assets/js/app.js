9695ac67057a12c01c93151edbfbca9e8594b6f2
Mouad ZIANI <mouad.ziani1997@gmail.com>
2019-03-04 17:33:18 +0100

    feat: finally finished responsive version enjoy :)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
 	$( ".datepicker" ).datepicker();
-	$(".owl-carousel").owlCarousel();
+	$(".owl-carousel").owlCarousel({
+		responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:true
+	        },
+	        600:{
+	            items:2,
+	            nav:false
+	        },
+	        1000:{
+	            items:3,
+	            nav:true,
+	            loop:false
+	        }
+	    }
+	});
 });
