2c3983668fc33a31568064ca29bfd111df6bed6d
Mouad ZIANI <mouad.ziani1997@gmail.com>
2019-03-09 17:17:23 +0100

    feat: adding multiple rows carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	$( ".datepicker" ).datepicker();
 	$(".owl-carousel").owlCarousel({
 		responsiveClass:true,
+		rows: true,
+		rowsCount: 2,
 	    responsive:{
 	        0:{
 	            items:1,
@@ -18,4 +20,26 @@ $(document).ready(function() {
 	        }
 	    }
 	});
+
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		slidesPerColumn: 2,
+		spaceBetween: 20,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+	        nextEl: '.swiper-button-next',
+	        prevEl: '.swiper-button-prev',
+      	}
+    });
+    $(window).resize(function() {
+	  var ww = $(window).width();
+	  if (ww>1000) swiper.params.slidesPerView = 3;
+	  if (ww>468 && ww<=1000) swiper.params.slidesPerView = 2;
+	  if (ww<=468) swiper.params.slidesPerView = 1;
+	  swiper.update();
+	})
+	$(window).trigger('resize');
 });
